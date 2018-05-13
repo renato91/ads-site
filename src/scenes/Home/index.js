178dd4fd22009875ad58f9f8acd70d0de396e57d
Renato Oliveira <renato_oliveira91@hotmail.com>
2018-05-13 21:16:42 +0000

    Initial commit

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Um designer deveria o quê?</h1>  
+        <p>Você já parou e pensou: Qual serão os próximos passos que devo dar para alcançar o próximo nível da minha carreira? Se sim, você está no lugar certo! Um designer deveria é um projeto que trás designers renomados para dar insights de caminhos, ferramentas, materiais de estudos, tudo aquilo que você deveria saber para alcançar seu próximo nível. E tudo isso de uma forma estrovertida, e muita história boa pra ser compartilhada!</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
